e5d12c0cb3e718e25efb05947b86170c921756fc
Eric Mugerwa <eric@rocketphp.io>
2015-09-01 19:40:11 +0100

    Add dependency status badge

--- test/LoggerTestCase.php
+++ test/LoggerTestCase.php
@@ -1,7 +1,16 @@
-<?php namespace RocketPHPTest\Logger;
-/** 
- * Logger Test Case
+<?php
+/**
+ * RocketPHP (http://rocketphp.io)
  *
+ * @package   RocketPHP
+ * @link      https://github.com/rocketphp/logger
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+
+namespace RocketPHPTest\Logger;
+
+/** 
+ * Test case for Logger
  */ 
 abstract class LoggerTestCase
 extends \PHPUnit_Framework_TestCase
